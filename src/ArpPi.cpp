2121d49387d70b6966565277f0ff92551ec56048
Ty Qualters <12106131+SalmonSeasoning@users.noreply.github.com>
2018-01-01 22:28:50 -0600

    till the future

--- src/ArpPi.cpp
+++ src/ArpPi.cpp
@@ -6,6 +6,6 @@ int main(){
     string ipaddress = "";
     cout << "Enter the target external IPv4 address: ";
     cin >> ipaddress; // get IPv4 address
-
+    system("python arp.py");
     return 0;
 }
