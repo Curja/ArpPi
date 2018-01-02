349bf41039f325e7cbd3b271fb03a85b720d6acc
Ty Qualters <12106131+SalmonSeasoning@users.noreply.github.com>
2018-01-02 11:20:35 -0600

    xd

--- src/ArpPi.cpp
+++ src/ArpPi.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <fstream>
 #include <Python.h>
 
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
+    ifstream ifs("arp.py");
+    string content ( (istreambuf_iterator<char>(ifs)), (istreambuf_iterator<char>()) );
+    
+
     string ipaddress = "";
     cout << "Enter the target external IPv4 address: ";
     cin >> ipaddress; // get IPv4 address
     Py_Initialize();
-    
+    FILE *fd = fopen("arp.py", "r");
+    PyRun_SimpleFileEx(fd, "arp.py", 1);
     Py_Finalize();
     return 0;
 }
